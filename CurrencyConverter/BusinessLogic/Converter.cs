56a7e9ce07d689a3d164ab5b0d6a17372600ccef
rarcz29 <rafalcz29@gmail.com>
2020-10-07 14:50:56 +0200

    Add Converter class to business logic layer

---
+++ CurrencyConverter/BusinessLogic/Converter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CurrencyConverter.BusinessLogic
+{
+    class Converter
+    {
+        public decimal Convert()
+        {
+            throw new NotImplementedException();
+        }
+
+        private decimal ConvertToPln()
+        {
+            throw new NotImplementedException();
+        }
+
+        private decimal ConvertFromPln()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
