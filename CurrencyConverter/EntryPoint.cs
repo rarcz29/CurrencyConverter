79afa9b30d94243db8712d590d8e67a5f1933c0a
rarcz29 <rafalcz29@gmail.com>
2020-10-07 14:31:01 +0200

    Add project to the solution.

---
+++ CurrencyConverter/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CurrencyConverter
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
