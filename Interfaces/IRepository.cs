c00d799190f58e37db32b9956b8771b89f855a67
rarcz29 <rafalcz29@gmail.com>
2020-10-12 11:28:46 +0200

    Refactoring

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CurrencyConverter.DataAccess.Repositories
+{
+    public interface IRepository<T>
+    {
+        T Get(object id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAllSavedData();
+        T GetSavedData(string id);
+    }
+}
