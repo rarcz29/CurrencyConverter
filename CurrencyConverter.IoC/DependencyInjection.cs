79bc905a5b72943bdedda0c4dc0ead7705c7c115
rarcz29 <rafalcz29@gmail.com>
2020-10-12 11:50:23 +0200

    Update

---
+++ CurrencyConverter.IoC/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CurrencyConverter.IoC
+{
+    class DependencyInjection
+    {
+        public ServiceProvider AppServiceProvider { get; set; }
+
+        public DependencyInjection()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddXmlDataConnector()
+                .AddCurrencyConverter()
+                .AddSingleton<IUserInterface, UserInterface>()
+                .BuildServiceProvider();
+        }
+    }
+}
